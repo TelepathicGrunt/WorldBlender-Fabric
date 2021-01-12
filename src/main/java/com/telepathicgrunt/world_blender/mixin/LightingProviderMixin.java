169315394f359cb70638f97fe96f701672401c2c
Alex Sarnese <40846040+TelepathicGrunt@users.noreply.github.com>
2021-01-12 15:05:37 -0500

    special mixin by shartte that prevents structures from crashing the lighting threads
    https://github.com/AppliedEnergistics/Applied-Energistics-2/pull/ 4935

---
+++ src/main/java/com/telepathicgrunt/world_blender/mixin/LightingProviderMixin.java
@@ -0,0 +1,31 @@
+package com.telepathicgrunt.world_blender.mixin;
+
+import com.telepathicgrunt.world_blender.WBIdentifiers;
+import com.telepathicgrunt.world_blender.dimension.EnderDragonFightModification;
+import net.minecraft.block.pattern.BlockPattern;
+import net.minecraft.entity.boss.dragon.EnderDragonFight;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.chunk.light.LightingProvider;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LightingProvider.class)
+public class LightingProviderMixin {
+
+	/**
+	 * Prevents lighting crashes that is next to impossible to debug. Special thanks to shartte for
+	 * figuring out the cause and allowing me to use his mixin workaround!
+	 *
+	 * @author shartte - https://github.com/AppliedEnergistics/Applied-Energistics-2/pull/4935/files
+	 * @reason Required to make sure structures that replaces light blocks does not crash servers
+	 */
+	@Inject(method = "addLightSource", at = @At("HEAD"), cancellable = true)
+	public void onBlockEmissionIncrease(BlockPos blockPos, int lightLevel, CallbackInfo ci) {
+		if (lightLevel == 0) {
+			ci.cancel();
+		}
+	}
+}
