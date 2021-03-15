7b10ac55cecdc975b73b0831397a6362c729a16f
Alex Sarnese <40846040+TelepathicGrunt@users.noreply.github.com>
2021-03-15 18:45:23 -0400

    Made biomes a bit bigger in World Blender's dimension.

--- src/main/java/com/telepathicgrunt/worldblender/dimension/MainBiomeLayer.java
+++ src/main/java/com/telepathicgrunt/worldblender/dimension/MainBiomeLayer.java
@@ -32,19 +32,19 @@ public class MainBiomeLayer implements InitLayer
 	@Override
 	public int sample(LayerRandomnessSource noise, int x, int z)
 	{
-		double perlinNoise = perlinGen.sample(x * 0.1D, z * 0.1D, false);
-		double perlinNoise2 = perlinGen.sample(x * 0.08D + 1000, z * 0.08D + 1000, false);
+		double perlinNoise = perlinGen.sample(x * 0.055D, z * 0.055D, false);
+		double perlinNoise2 = perlinGen.sample(x * 0.075D + 1000, z * 0.075D + 1000, false);
 		
 //		max = Math.max(max, perlinNoise);
 //		min = Math.min(min, perlinNoise);
 //		AllTheFeatures.LOGGER.log(Level.DEBUG, "Max: " + max +", Min: "+min + ", perlin: "+perlinNoise);
 		
 		
-		if(perlinNoise > 0.53) {	
+		if(perlinNoise > 0.51) {
 			return this.dynamicRegistry.getRawId(this.dynamicRegistry.get(WBIdentifiers.MOUNTAINOUS_BLENDED_BIOME_ID));
 		}
-		else if(perlinNoise > -0.58) {	
-			if(perlinNoise2 < -0.75) {	
+		else if(perlinNoise > -0.6) {
+			if(perlinNoise2 < -0.62) {
 				return this.dynamicRegistry.getRawId(this.dynamicRegistry.get(WBIdentifiers.COLD_HILLS_BLENDED_BIOME_ID));
 			}
 			else {
@@ -52,7 +52,7 @@ public class MainBiomeLayer implements InitLayer
 			}
 		}
 		else {	
-			return noise.nextInt(100)/800D + perlinNoise%0.4D > -0.2D ?
+			return noise.nextInt(100) / 800D + perlinNoise % 0.4D > -0.2D ?
 					this.dynamicRegistry.getRawId(this.dynamicRegistry.get(WBIdentifiers.OCEAN_BLENDED_BIOME_ID)) :
 					this.dynamicRegistry.getRawId(this.dynamicRegistry.get(WBIdentifiers.FROZEN_OCEAN_BLENDED_BIOME_ID));
 		}
